10186a68d2bc579232ebbb6aca9d09b69ec093a1
Vlad Mikhalchuk <napitok.94@mail.ru>
2021-07-29 18:28:23 +0400

    Quizz done!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import Quizz from './components/contents/Quizz'
+import './App.css'
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      
-      </header>
+      <Quizz />
     </div>
   );
 }
