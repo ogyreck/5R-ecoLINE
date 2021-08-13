40b893cdfc48e977fb6a50f2d2ceca48266356f6
Kostya <romanovkostya1999@gmail.com>
2021-08-13 22:41:22 +0400

    делаю главную

--- src/components/contents/main/Main.jsx
+++ src/components/contents/main/Main.jsx
@@ -7,7 +7,7 @@ import Quiz from "../main/Quiz/Quiz"
 import "./main.css";
 
 import Subscribe from "../shop/Subscribe/Subscribe";
-import Numbers from "./Numbers/Numbers";
+// import Numbers from "./Numbers/Numbers";
 import Purpose from "./Purpose/Purpose";
 import Concept from "./Concept/Concept";
 import Blog from "./Blog/Blog";
@@ -31,28 +31,16 @@ export default class Main extends Component {
         <main>
             <section className="main">
               <div className="container">
-                <h1 className="main__title">Стань частью больших изменений</h1>
-                <span className="main__subtitle">Поможем сформировать твой персональный план по “спасению мира”</span>
-                <div className="main__btn">
-                  <button className="main__btn-info">Читать</button>
-                  <Link to="/shop"> <button className="main__btn-shop">В магазин</button></Link>
-                </div>
+              <h1 className="main__title">“Мы хотим стать магазином, который будет создавать стиль и гармонию в Вашем доме, а не мусор”</h1>
+                {/* <span className="main__subtitle">Поможем сформировать твой персональный план по “спасению мира”</span> */}
+                {/* <div className="main__btn"> */}
+                  {/* <button className="main__btn-info">Читать</button> */}
+                  <Link to="/shop"><button className="main__btn-shop">В магазин</button></Link>
+                {/* </div> */}
               </div>
             </section>
-          <Numbers />
+          {/* <Numbers /> */}
           <Slider />
-          {/* <section className="video">
-            <div className="container video__row">
-              <div className="block__video">
-                <img className="block__video-url" src="../img/video_preloader.jpg" alt="" />
-                <iframe className="block__video-url" src="https://www.youtube.com/embed/gJAp5tWiN8Q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-              </div>
-              <div className="block__text">
-                <div className="block__text-motivation">Жить экологично - это <br />легко, полезно и выгодно!</div>
-                <button className="block__text-btn" onClick={() => this.click()}>В магазин</button>
-              </div>
-            </div>
-          </section> */}
           <Quiz />
           <Purpose />
           <Concept />
--- src/components/contents/main/Slider/Slider.jsx
+++ src/components/contents/main/Slider/Slider.jsx
@@ -6,6 +6,7 @@ import './slider.css';
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css"
 import "swiper/components/pagination/pagination.min.css"
+import { Link } from 'react-router-dom';
 
 const Slider = () => {
     
@@ -14,33 +15,53 @@ const Slider = () => {
     return (
         <section className="slider">
             <div className="container">
-                <h2 className="slider__title">Жить экологично - это легко, полезно и выгодно!</h2>
+                {/* <h2 className="slider__title">Жить экологично - это легко, полезно и выгодно!</h2> */}
                 <Swiper
                     navigation={true}
                     pagination={true}
                     // slidesPerView={1}
                     className="mainSlider"
-                    loop={true}
                 >
                     <SwiperSlide>
-                        <img className="slide__img" src="/img/slider/slide_1.png" alt="" />
-                        <div className="slide__info">
-                            <p className="slide__text">Стакан складной Stojo</p>
-                            <button className="slide__btn">Смотреть</button>
+                        <div className="slider-wrapper">
+                            <span className="slider__new">NEW</span>
+                            <Link to="/shop"><span className="slider__more">Смотреть еще<img src="img/slider/arrow.svg" alt="" /></span></Link>
+                        </div>
+                        <div className="slider-info__wrapper">
+                            <img className="slide__img" src="/img/slider/slide_2.png" alt="" />
+                            <div className="slide__info">
+                                <p className="slide__text">Стакан складной Stojo</p>
+                                <p className="slide__price">1 200 ₽</p>
+                                <Link to="/shop" className="slide__btn">Смотреть</Link>
+                            </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img className="slide__img" src="/img/slider/slide_1.png" alt="" />
-                        <div className="slide__info">
-                            <p className="slide__text">Стакан складной Stojo</p>
-                            <button className="slide__btn">Смотреть</button>
+                        <div className="slider-wrapper">
+                            <span className="slider__new">NEW</span>
+                            <span className="slider__more">Смотреть еще<img src="img/slider/arrow.svg" alt="" /></span>
+                        </div>
+                        <div className="slider-info__wrapper">
+                            <img className="slide__img" src="/img/slider/slide_2.png" alt="" />
+                            <div className="slide__info">
+                                <p className="slide__text">Стакан складной Stojo</p>
+                                <p className="slide__price">1 200 ₽</p>
+                                <Link to="/shop" className="slide__btn">Смотреть</Link>
+                            </div>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img className="slide__img" src="/img/slider/slide_1.png" alt="" />
-                        <div className="slide__info">
-                            <p className="slide__text">Стакан складной Stojo</p>
-                            <button className="slide__btn">Смотреть</button>
+                        <div className="slider-wrapper">
+                            <span className="slider__new">NEW</span>
+                            <span className="slider__more">Смотреть еще<img src="img/slider/arrow.svg" alt="" /></span>
+                        </div>
+                        <div className="slider-info__wrapper">
+                            <img className="slide__img" src="/img/slider/slide_2.png" alt="" />
+                            <div className="slide__info">
+                                <p className="slide__text">Стакан складной Stojo</p>
+                                <p className="slide__price">1 200 ₽</p>
+                                <Link to="/shop" className="slide__btn">Смотреть</Link>
+                            </div>
                         </div>
                     </SwiperSlide>
 
