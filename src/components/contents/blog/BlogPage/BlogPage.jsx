a12bd2bef4e5417db1409b2ae9387e43326f9481
Andrfey <andrej.peredvigin@mail.ru>
2021-08-15 16:57:09 +0400

    блог страница начало

---
+++ src/components/contents/blog/BlogPage/BlogPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './blogPage.css';
+import HeaderShopContainer from "../../../header/Header_shopContainer";
+
+
+const BlogPage = () =>{
+    return(
+        <div>
+            <HeaderShopContainer/>
+        </div>
+    )
+
+
+}
+
+export default BlogPage;
